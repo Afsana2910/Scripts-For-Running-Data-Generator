d6da6cc5bf507f5ff05931d47cca3e73cffbec6c
Afsana Khan <58783192+Afsana2910@users.noreply.github.com>
2021-08-05 16:40:11 +0300

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import json
 def runCommandline(device_config):
-    command = "/usr/bin/java -cp /home/afsana/Fall2020/Thesis/iosynth/target/iosynth-0.0.7.jar net.iosynth.Mqtt -c /home/afsana/Fall2020/Thesis/iosynth/target/config-mqtt.json -d /home/afsana/Fall2020/Thesis/configurations/"+device_config
+    command = "/usr/bin/java -cp ~/iosynth/target/iosynth-0.0.7.jar net.iosynth.Mqtt -c ~/iosynth/target/config-mqtt.json -d ~/configurations/"+device_config
     print(os.system(command))
 
 
@@ -29,12 +29,12 @@ process()
 weekno = datetime.datetime.today().weekday()
 currentHour = str(datetime.datetime.now().hour)
 if weekno < 5:
-	with open("/home/afsana/Fall2020/Thesis/configurations/configure.json", "r") as f:
+	with open("~/configurations/configure.json", "r") as f:
         	data = json.load(f)
 	config_file = str(data[currentHour])
 	runCommandline(config_file)
 else:  # 5 Sat, 6 Sun
-	with open("/home/afsana/Fall2020/Thesis/configurations/configure.json", "r") as f:
+	with open("~/configurations/configure.json", "r") as f:
         	data = json.load(f)
 	config_file = str(data[currentHour])
 	runCommandline(config_file)
